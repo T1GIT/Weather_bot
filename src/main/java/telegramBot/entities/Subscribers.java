b49534506c6c6135a3b253968ad1f6338c0a6264
T1MON <derbindima5@gmail.com>
2020-12-09 13:09:29 +0400

    ver. 1.0 Added all modes

--- src/main/java/telegramBot/entities/Subscribers.java
+++ src/main/java/telegramBot/entities/Subscribers.java
@@ -7,18 +7,12 @@ import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class Subscribers extends Thread {
-    private static final Calendar day = Calendar.getInstance();
-    private static final int[] time = new int[]{9, 0, 0};
-    private static final long period = TimeUnit.MILLISECONDS.convert(10, TimeUnit.SECONDS);
+    private static final long period = TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS);
     private final HashSet<User> subscribers;
     private final Timer timer;
 
     public Subscribers() {
-        System.out.println("INTITIALISING");
         subscribers = new HashSet<>();
-        day.set(Calendar.HOUR_OF_DAY, time[0]);
-        day.set(Calendar.MINUTE, time[1]);
-        day.set(Calendar.SECOND, time[2]);
         timer = new Timer();
     }
 
@@ -37,12 +31,8 @@ public class Subscribers extends Thread {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                System.out.println("SCHEDULE");
-                for (User user: subscribers) {
-                    System.out.println("SENDING >>>>>>>>>>> " + user.getName());
-                    user.sendCurrent();
-                }
+                for (User user: subscribers) user.sendCurrent();
             }
-        }, day.getTime(), period);
+        }, 0, period);
     }
 }
